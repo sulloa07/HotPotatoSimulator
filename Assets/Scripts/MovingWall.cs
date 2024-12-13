9ab435e8b36b4d75b26eb19732d5dbebece76a4c
JerBearJets <145627532+JerBearJets@users.noreply.github.com>
2024-12-12 16:09:30 -0800

    Walls are Synced

--- Assets/Scripts/MovingWall.cs
+++ Assets/Scripts/MovingWall.cs
@@ -1,22 +1,36 @@
 using UnityEngine;
-
-public class MovingWall : MonoBehaviour
+using Alteruna;
+public class MovingWall : AttributesSync
 {
     [SerializeField]
+    [SynchronizableField]
     private float moveSpeed = 1f; // Speed of the wall movement
+
     [SerializeField]
+    [SynchronizableField]
     private float moveHeight = 2f; // Height the wall moves up and down
+
     [SerializeField]
+    [SynchronizableField]
     private float[] waitTimes = { 10f, 5f, 8f, 5f, 12f, 10f, 5f, 6f}; // Array of wait times before moving again
+
     [SerializeField]
     private AudioClip openAudio; // Audio clip for opening
     [SerializeField]
     private AudioClip closeAudio; // Audio clip for closing
 
+    [SynchronizableField]
     private Vector3 startPos;
+
+    [SynchronizableField]
     private bool movingDown = true;
+
+    [SynchronizableField]
     private int currentWaitIndex = 0;
+
+    [SynchronizableField]
     private float timer;
+
     private AudioSource audioSource;
 
     void Start()
